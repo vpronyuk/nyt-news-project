d37300e627d2c19840ab67aa070cf14275cbd5dd
AlexanderShevchenko112 <aleks.shevchenko.2013@gmail.com>
2023-03-03 14:04:12 +0200

    add week forecast

--- src/js/weather.js
+++ src/js/weather.js
@@ -6,8 +6,27 @@ const dateEl = document.querySelector('.date');
 const weekDay = document.querySelector(`.week-day`);
 const monthAndYear = document.querySelector(`.month-year`);
 const weatherCard = document.querySelector(`.weather-card`);
-const weekWeatherBtn = document.querySelector(`week-weather-btn`);
+const weekWeatherBtn = document.querySelector(`.week-weather-btn`);
+const todayWeatherBtn = document.querySelector(`.today-forecast-btn`);
+const weekWeatherCard = document.querySelector(`.week-forecast`);
+const weekWeatherList = document.querySelector(`.week-forecast-list`);
 const API_KEY = 'e47ed3661eb61aefd3a2374eeeb0d76d';
+
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
 
 function getWeatherData(latitude, longitude) {
   const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
@@ -21,11 +40,10 @@ function getWeatherData(latitude, longitude) {
     })
     .then(data => {
       const iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-      cityName.textContent = data.name;
+      cityName.insertAdjacentHTML(`beforeend`, data.name);
       temperature.innerHTML = `${Math.round(data.main.temp)}&deg`;
       description.textContent = data.weather[0].description;
       weatherIcon.setAttribute('src', iconUrl);
-      // console.log(cityName);
     })
     .catch(error => {
       console.log(error);
@@ -63,21 +81,7 @@ navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
 function displayDate() {
   const date = new Date();
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
+
   const dayOfWeek = daysOfWeek[date.getDay()];
   const dayOfMonth = date.getDate().toString().padStart(2, '0');
   const month = months[date.getMonth()];
@@ -88,8 +92,65 @@ function displayDate() {
 
 displayDate();
 
-weekWeatherBtn.addEventListener(`submit`, getWeekWeather);
+weekWeatherBtn.addEventListener(`click`, () => {
+  weatherCard.style.display = 'none';
+  weekWeatherCard.style.display = 'block';
+  const newApiKey = `aded7e3553934d64aa846631cabf27f3`;
+  function getCurrentLocation() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          resolve({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        error => {
+          reject(error);
+        }
+      );
+    });
+  }
 
-function getWeekWeather() {
-  //
-}
+  function getWeatherData(location) {
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${location.latitude}&lon=${location.longitude}&key=${newApiKey}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => data.data);
+  }
+
+  function displayWeatherData(weatherData) {
+    const weekWeatherMarkup = weatherData
+      .map(day => {
+        const date = new Date(day.valid_date).toLocaleDateString();
+        const minTemp = day.min_temp;
+        const maxTemp = day.max_temp;
+        const descriptionWeek = day.weather.description;
+        const iconWeek = day.weather.icon;
+        return `
+        <li class="week-weather-item">
+          <div class="week-weather-date">${date}</div>
+          <div class="week-weather-body">
+            <div class="week-weather-icon">
+              <img src="https://www.weatherbit.io/static/img/icons/${iconWeek}.png" width=60px>
+            </div>
+            <div class="week-weather-temp">${minTemp}° - ${maxTemp}°</div>
+            <div class="week-weather-desc">${descriptionWeek}</div>
+          </div>
+        </li>
+      `;
+      })
+      .join('');
+    weekWeatherList.innerHTML = weekWeatherMarkup;
+  }
+
+  getCurrentLocation()
+    .then(location => getWeatherData(location))
+    .catch(() => getWeatherData({ latitude: '50.45466', longitude: '30.5238' }))
+    .then(weatherData => displayWeatherData(weatherData));
+});
+
+todayWeatherBtn.addEventListener(`click`, () => {
+  weekWeatherCard.style.display = 'none';
+  weatherCard.style.display = 'block';
+});
