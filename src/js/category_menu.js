6d76a16e0588691247d076a3bd9fa7dc05a0f9fa
Alexander Vyzhymov <vizimov@gmail.com>
2023-03-04 13:43:35 +0200

    finished-desk-menu

--- src/js/category_menu.js
+++ src/js/category_menu.js
@@ -3,13 +3,16 @@ const categoriesList = document.querySelector('.category_list');
 const dropDownMenu = document.querySelector('.dropdown');
 const dropDownContent = document.querySelector('.dropdown_content');
 const mobileCatList = document.querySelector('.mobile_category_list');
-const otherBtn = document.querySelector('.category_other_btn');
+const otherBtn = document.querySelector('.category_btn');
 const mobCatBtn = document.querySelector('.mobile_category_btn');
-const catBtnIcon = document.querySelector('.category_icon');
+const mobCatBtnIcon = document.querySelector('.category_mob_icon');
 const mobBtnSpan = document.querySelector('.mob-btn-span');
+const catBtnIcon = document.querySelector('.category_icon');
+const deskSpanBtn = document.querySelector('.desk-btn-span');
 
 const content = document.querySelector('.content');
-console.log(content);
+
+//---------------------- Mobile categories menu -------------------------------------//
 
 mobCatBtn.addEventListener('click', onClickMobileBtn);
 mobileCatList.addEventListener('click', onClickMobileCat);
@@ -20,9 +23,9 @@ function onClickMobileBtn(event) {
 
   if (mobBtnSpan.textContent === 'Categories') {
     mobCatBtn.classList.toggle('is-active');
-    catBtnIcon.classList.toggle('rotate');
+    mobCatBtnIcon.classList.toggle('rotate');
   } else {
-    catBtnIcon.classList.toggle('rotate');
+    mobCatBtnIcon.classList.toggle('rotate');
   }
 }
 
@@ -35,8 +38,8 @@ function onClickMobileCat(event) {
   const text = event.target.dataset.name;
   mobileCatList.classList.add('category_hidden');
   mobCatBtn.classList.add('is-active');
-  catBtnIcon.classList.remove('rotate');
-  catBtnIcon.style.fill = 'white';
+  mobCatBtnIcon.classList.remove('rotate');
+  mobCatBtnIcon.style.fill = 'white';
 
   console.log(query);
 
@@ -98,24 +101,26 @@ function createMarkup({ section, display_name }) {
   `;
 }
 
-categoriesList.addEventListener('click', e => {
-  const query = e.target.dataset.section;
+// categoriesList.addEventListener('click', e => {
+//   const query = e.target.dataset.section;
 
-  fetch(
-    `https://api.nytimes.com/svc/news/v3/content/inyt/${query}.json?api-key=${API_KEY}`
-  )
-    .then(res => res.json())
-    .then(data => {
-      if (data.results === null) {
-        content.innerHTML = '';
-      } else {
-        const cards = data.results.reduce((markup, card) => {
-          return markup + createCard(card);
-        }, '');
-        content.innerHTML = cards;
-      }
-    });
-});
+//   if (e.target.textContent === 'Other') {
+//     fetch(
+//       `https://api.nytimes.com/svc/news/v3/content/inyt/${query}.json?api-key=${API_KEY}`
+//     )
+//       .then(res => res.json())
+//       .then(data => {
+//         if (data.results === null) {
+//           content.innerHTML = '';
+//         } else {
+//           const cards = data.results.reduce((markup, card) => {
+//             return markup + createCard(card);
+//           }, '');
+//           content.innerHTML = cards;
+//         }
+//       });
+//   }
+// });
 
 function createCard({ title, thumbnail_standard }) {
   return `
@@ -124,4 +129,35 @@ function createCard({ title, thumbnail_standard }) {
   <img src="${thumbnail_standard}">
   </div>
   `;
+}
+
+//------------------------- categories menu -------------------------------------//
+
+otherBtn.addEventListener('click', onClickOtherBtn);
+categoriesList.addEventListener('click', onClickCatBtn);
+
+function onClickOtherBtn(event) {
+  dropDownContent.classList.toggle('category_hidden');
+  catBtnIcon.classList.toggle('rotate');
+  otherBtn.classList.toggle('is-active');
+}
+
+function onClickCatBtn(event) {
+  if (
+    event.target.classList.contains('category_btn') ||
+    event.target.classList.contains('desk-btn-span') ||
+    event.target.classList.contains('category_icon')
+  ) {
+    return;
+  }
+
+  if (!dropDownContent.classList.contains('category_hidden')) {
+    dropDownContent.classList.add('category_hidden');
+    catBtnIcon.classList.remove('rotate');
+  }
+
+  console.log(event.target);
+  const query = event.target.dataset.section;
+  const text = event.target.dataset.name;
+  console.log(query);
 }
