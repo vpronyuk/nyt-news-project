4d3a6793cfe7369b92b466f8e20e35ef17c47dcd
Alexander Vyzhymov <vizimov@gmail.com>
2023-03-09 23:00:45 +0200

    added-filter

--- src/js/category_menu.js
+++ src/js/category_menu.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const API_KEY = 'AKwAkjVAbzaYm1bK9yzcr2BnwjHsxavz';
 const BASE_URL = 'https://api.nytimes.com/svc/';
-const CATEGORY_NEWS = 'news/v3/content/inyt/';
+const CATEGORY_NEWS = 'news/v3/content/all/';
 const CATEGORY_LIST = `https://api.nytimes.com/svc/news/v3/content/section-list.json?`;
 
 const mobileMenu = document.querySelector('.mobile_category_menu');
@@ -21,6 +21,12 @@ const emptyPage = document.querySelector('.empty');
 const darkModeDeskCheckbox = document.querySelector('.checkbox-header__input');
 const darkModeMobCheckbox = document.querySelector('.mob-menu__checkbox-input');
 const weatherCard = document.querySelector('.weather-card');
+const dateCalendarInput = document.querySelector('.calendar__input');
+
+const noImage =
+  'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg';
+const noNews =
+  'https://cdn.dribbble.com/users/2382015/screenshots/6065978/media/8b4662f8023e4e2295f865106b5d3aa7.gif';
 
 mobileCatList.classList.remove('category_hidden');
 mobileCatList.classList.add('category_mobile_hidden');
@@ -89,7 +95,6 @@ function closeMobileMenu(e) {
 }
 
 function onClickMobileCat(event) {
-  console.dir(event.target);
   if (event.target.tagName !== 'BUTTON') {
     return;
   }
@@ -118,6 +123,8 @@ function onClickMobileCat(event) {
       emptyPage.style.display = 'none';
       weatherCard.style.display = 'block';
       newsList.innerHTML = cards;
+
+      observer.observe(dateCalendarInput, config);
     }
   });
 }
@@ -150,7 +157,7 @@ getCategoryList().then(response => {
 
   const catMenuBtnList = document.querySelectorAll('.cat-menu-btn');
   const darkMode = JSON.parse(localStorage.getItem('isDarkMode'));
-  console.log(darkMode);
+
   if (darkMode) {
     for (let btn of catMenuBtnList) {
       btn.classList.add('dark-categories-btn');
@@ -200,8 +207,6 @@ function onClickOtherBtn(event) {
 
 function closeDesktopMenu(e) {
   const withinDesktopMenu = e.composedPath().includes(dropDownMenu);
-
-  console.log(withinDesktopMenu);
 
   if (
     !withinDesktopMenu &&
@@ -269,6 +274,8 @@ function onClickCatBtn(event) {
       emptyPage.style.display = 'none';
       weatherCard.style.display = 'block';
       newsList.innerHTML = cards;
+
+      observer.observe(dateCalendarInput, config);
     }
   });
 }
@@ -282,7 +289,7 @@ function createCard({
   url,
 }) {
   const id = uuidv4();
-  const imageUrl = multimedia?.[2]?.url || '';
+  const imageUrl = multimedia?.[2]?.url || noImage;
   const MAX_SNIPPET_LENGTH = 110;
   if (abstract.length > MAX_SNIPPET_LENGTH) {
     abstract = abstract.slice(0, MAX_SNIPPET_LENGTH - 3) + '...';
@@ -292,7 +299,7 @@ function createCard({
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear().toString();
   const formattedDate = `${day}/${month}/${year}`;
-  return `<li class="list-news__item" data-id="${id}" >
+  return `<li class="list-news__item" data-id="${id}" data-pub_date="${formattedDate}" >
             <article class="item-news__article">
               <div class="item-news__wrapper-img">
                 <img class="item-news__img" src="${imageUrl}" alt="photo">
@@ -302,12 +309,16 @@ function createCard({
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" stroke="#4440f7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95C22 5.216 19.761 3 17 3s-5 3-5 3s-2.239-3-5-3Z"/></svg>
                   </button>
                   <button class="article_flag--remove is-hidden"><span class="article_flag_text">Remove from favorite</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="#4b48da" stroke="#4b48da" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95C22 5.216 19.761 3 17 3s-5 3-5 3s-2.239-3-5-3Z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="#4b48da" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95C22 5.216 19.761 3 17 3s-5 3-5 3s-2.239-3-5-3Z"/></svg>
                   </button>
                 </div>
               </div>
-              <h2 class="item-news__title">${title}</h2>
-              <p class="item-news__description">${abstract}</p>
+              <h2 class="item-news__title">${
+                title === '' ? 'Title not found' : title
+              }</h2>
+              <p class="item-news__description">${
+                abstract === '' ? 'Description not found' : abstract
+              }</p>
               <div class="item-news__info">
                 <span class="item-news__info-date">${formattedDate}</span>
                 <a class="item-news__info-link" href="${url}" target="_blank" rel="noreferrer noopener">Read more</a>
@@ -322,7 +333,6 @@ function readmoreHandler(e) {
     readMoreLink.setAttribute('data-is-read', true);
 
     const ulItem = e.target.parentElement.parentElement.parentElement;
-    console.log(ulItem);
     const read = document.createElement('p');
     read.innerText = 'Already read';
     read.classList.add('have-read');
@@ -335,3 +345,44 @@ function readmoreHandler(e) {
 newsList.addEventListener('click', readmoreHandler);
 
 // localStorage.setItem('cards', '[]');
+
+//-------------------------------- Відслідквання зміни data атрибуту на інпуті ------------------//
+
+const config = {
+  attributes: true,
+};
+
+let mutationCount = 0;
+
+const getFilterNewsByDate = function (el, observer) {
+  for (const mutation of el) {
+    if (mutation.type === 'attributes' && mutationCount === 0) {
+      mutationCount += 1;
+    } else {
+      const date = dateCalendarInput.dataset.date.split('.').join('/');
+      weatherCard.style.display = 'none';
+
+      const newsArray = Array.prototype.slice.call(newsList.childNodes);
+      const filteredNewsArray = newsArray.filter(
+        el => el.dataset.pub_date === date
+      );
+      if (filteredNewsArray.length > 0) {
+        newsList.childNodes.forEach(el => {
+          if (el.dataset.pub_date !== date) {
+            el.style.display = 'none';
+          } else {
+            el.style.display = 'block';
+          }
+        });
+      } else {
+        newsList.childNodes.forEach(el => (el.style.display = 'none'));
+        newsList.insertAdjacentHTML(
+          'afterbegin',
+          `<li style="margin:0 auto"><img src="${noNews}" alt="no news"></img></li>`
+        );
+      }
+    }
+  }
+};
+
+const observer = new MutationObserver(getFilterNewsByDate);
